b9db320ea211446de7833ee93c6ea273945b38dc
Zeliha Gökoğlan <zelihagokoglan@gmail.com>
2025-09-03 16:29:10 +0300

    first commit

---
+++ Assets/Scripts/TileCell.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class TileCell : MonoBehaviour
+{
+    public Vector2Int cordinates {get; set;}
+    public Tile tile { get; set; }
+
+
+    public bool empty => tile == null;
+    public bool occupied =>tile != null;
+}
---
+++ Assets/Scripts/TileRow.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class TileRow : MonoBehaviour
+{
+   public TileCell [] cells { get; private set; }
+
+    private void Awake()
+    {
+        cells = GetComponentsInChildren<TileCell>();
+    }
+}
